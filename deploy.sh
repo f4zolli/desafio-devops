26bc97a8ffb9c3c93003bacac138954342211d89
Matheus Fazolli <106924623+f4zolli@users.noreply.github.com>
2025-01-22 00:09:19 -0300

    feat: added sudo command

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ sudo apt update
 sudo apt install docker.io docker-compose -y
 
 echo "Construindo e subindo o container..."
-docker-compose up --build -d
+sudo docker-compose up --build -d
 
 echo "Aplicação disponível em: http://localhost:8080"
